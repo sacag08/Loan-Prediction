b318d8abd1210336c2aa452487ab13e007a559e9
Sacag08 <sachitagarwal98@gmail.com>
2024-09-19 23:38:40 +0530

    Requirements and package creation

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(filepath:str)->List[str]:
+    requirements = []
+    with open(filepath) as fileobj:
+        requirements = fileobj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+    print(requirements)
+    return requirements
+
+setup(
+    name = "Loan Prediction",
+    version = "0.0.1",
+    author = "Sachit",
+    author_email = "sachitagarwal98@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
